b227d0cae8fc74a5c3d136961c325e886fb7384c
Yasin <yasin-93@hotmail.de>
2021-09-20 21:18:01 +0200

    added bundler and minor changes

--- src/parts/organisms/EditorLayout/EditorLayout.util.ts
+++ src/parts/organisms/EditorLayout/EditorLayout.util.ts
@@ -1,22 +1,25 @@
 import { html } from 'lit';
-import { ToolboxButton } from '../../atoms/ToolboxButton/index.js';
 import { ToolGeneratorFunction } from './EditorLayout.types';
 
 export const generateTools: ToolGeneratorFunction = handleToolSelection => [
-  { title: 'Line 0', onClick: handleToolSelection, id: '0' },
-  { title: 'Line 1', onClick: handleToolSelection, id: '1' },
-  { title: 'Line 2', onClick: handleToolSelection, id: '2' },
-  { title: 'Line 3', onClick: handleToolSelection, id: '3' },
-  { title: 'Line 4', onClick: handleToolSelection, id: '4' },
-  { title: 'Line 5', onClick: handleToolSelection, id: '5' },
-  { title: 'Line 6', onClick: handleToolSelection, id: '6' },
+  { title: 'Tool 0', onClick: handleToolSelection, id: '0' },
+  { title: 'Tool 1', onClick: handleToolSelection, id: '1' },
+  { title: 'Tool 2', onClick: handleToolSelection, id: '2' },
+  { title: 'Tool 3', onClick: handleToolSelection, id: '3' },
+  { title: 'Tool 4', onClick: handleToolSelection, id: '4' },
+  { title: 'Tool 5', onClick: handleToolSelection, id: '5' },
+  { title: 'Tool 6', onClick: handleToolSelection, id: '6' },
 ];
 
 export const getButtonColumn = (
   tools: Array<any>,
   interval: [number, number]
 ) =>
-  tools.slice(interval[0], interval[1]).map((tool, index) => {
-    const toolboxButton = new ToolboxButton(tool);
-    return html`<span class="colum-${index % 2}">${toolboxButton}</span>`;
-  });
+  tools
+    .slice(interval[0], interval[1])
+    .map(
+      (tool, index) =>
+        html`<span class="colum-${index % 2}"
+          ><toolbox-button .props=${tool}></toolbox-button
+        ></span>`
+    );
