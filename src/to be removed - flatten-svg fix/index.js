eb68ef47466f8470ab48e59c156e82435d91e293
InFlameOus171 <49519354+InFlameOus171@users.noreply.github.com>
2021-12-22 18:38:00 +0100

    to be removed

---
+++ src/to be removed - flatten-svg fix/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let exports = {};
+Object.defineProperty(exports, "__esModule", { value: true });
+import { flattenSVG } from "./svg-to-paths";
+Object.defineProperty(exports, "flattenSVG", { enumerable: true, get: function () { return flattenSVG; } });
+export {flattenSVG}; 
+//# sourceMappingURL=index.js.map
