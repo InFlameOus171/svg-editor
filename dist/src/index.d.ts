4a7ec9bfa79921805f9186a07fd3f0355cfcad70
InFlameOus171 <49519354+InFlameOus171@users.noreply.github.com>
2022-02-01 10:46:24 +0100

    added template

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,10 +1,15 @@
 import './components/pages/SVGEditor';
+import './components/templates/EditorTemplate';
+import { EditorTemplateProps } from './components/templates/EditorTemplate/EditorTemplate.types';
 declare global {
     namespace JSX {
         interface IntrinsicElements {
             'svg-editor': SVGEditor;
+            'editor-template': EditorTemplate;
         }
         interface SVGEditor {
+        }
+        interface EditorTemplate extends EditorTemplateProps {
         }
     }
 }
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,2 +1,3 @@
 import './components/pages/SVGEditor';
+import './components/templates/EditorTemplate';
 //# sourceMappingURL=index.js.map
